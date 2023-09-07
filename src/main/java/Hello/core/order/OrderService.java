c4b1d9b98a7f2d66b457147d51dc2da3342f45a6
EunMin0120 <123084264+EunMin0120@users.noreply.github.com>
2023-09-07 14:38:12 +0900

    Delete src/main/java/Hello/core directory

--- src/main/java/Hello/core/order/OrderService.java
+++
@@ -1,8 +0,0 @@
-package hello.core.order;
-
-public interface OrderService {
-    Order createOrder(Long memberId, String itemName, int itemPrice);
-}
-
-//주문 도메인 전체를 보면
-// 1. 주문생성 - 회원 id, 상품명, 상품가격
